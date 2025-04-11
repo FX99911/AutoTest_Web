54fa376f3f09c818344ad2986b5aaa66ce36fe9f
Your Name <your.email@example.com>
2025-04-11 13:33:09 +0800

    完成了导入测试用例按钮

--- web_keys/read_excel/run_excel_to_py.py
+++ web_keys/read_excel/run_excel_to_py.py
@@ -1,35 +1,40 @@
 from web_keys.read_excel.red_excel_sace_json import *
 
-# 使用示例
-if __name__ == "__main__":
-    # 导入文件的路径文件路径
-    excel_url = f"{home}/cases_date/test_excel.xlsx"
-    # 测试用例运行模板路径
-    template_path = f"{home}/cases_date/test_case_template.py"
 
-    # 读取Excel文件（前两行）
-    data = read_single_excel(excel_url)
-    print("Excel前两行数据:")
-    print(data)
+def run_excel_to_py(lst):
 
-    # 读取Excel文件（从第四行开始）
-    data_from_fourth = read_excel_from_fourth_row(excel_url)
-    print("\nExcel从第四行开始的数据:")
-    for test_case in data_from_fourth:
-        print(test_case)
-        print()
+        # 导入文件的路径文件路径
+        excel_url = f"{home}/cases_date/"
+        # 测试用例运行模板路径
+        template_path = f"{home}/cases_date/test_case_template.py"
 
-    # 创建测试用例目录结构
-    third_dir = create_directories(data)
-    print(f"\n测试用例-第三级目录路径: {third_dir}")
 
-    # 创建截图目录结构
-    picture_dir = create_picture_directories(data)
-    print(f"\n测试截图-第三级目录路径: {picture_dir}")
+        for x in lst: # 循环 接收的列表（文件名）
+            excel_url_name = f'{excel_url}{x}' # 拼接成完整（路径+文件名）
 
-    # 使用模板创建测试文件
-    created_files = create_test_files_from_template(data_from_fourth, third_dir, data, template_path)
-    print("\n创建的文件列表:")
-    for file_path in created_files:
-        print(file_path)
+            # 读取Excel文件（前两行）
+            data = read_single_excel(excel_url_name)
+            print("Excel前两行数据:")
+            print(data)
+
+            # 读取Excel文件（从第四行开始）
+            data_from_fourth = read_excel_from_fourth_row(excel_url_name)
+            print("\nExcel从第四行开始的数据:")
+            for test_case in data_from_fourth:
+                print(test_case)
+                print()
+
+            # 创建测试用例目录结构
+            third_dir = create_directories(data)
+            print(f"\n测试用例-第三级目录路径: {third_dir}")
+
+            # 创建截图目录结构
+            picture_dir = create_picture_directories(data)
+            print(f"\n测试截图-第三级目录路径: {picture_dir}")
+
+            # 使用模板创建测试文件
+            created_files = create_test_files_from_template(data_from_fourth, third_dir, data, template_path)
+            print("\n创建的文件列表:")
+            for file_path in created_files:
+                print(file_path)
 
