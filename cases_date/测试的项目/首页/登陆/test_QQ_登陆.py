4e5634255783726ddb77b8fc43f2fdc72d1d4558
Your Name <your.email@example.com>
2025-04-18 18:46:27 +0800

    哈哈

---
+++ cases_date/测试的项目/首页/登陆/test_QQ_登陆.py
@@ -0,0 +1,4 @@
+# 测试用例数据
+test_data = {'打开_url_登录': ['https://www.qq.com', 'nan', 'nan']}
+
+# 后续可以在这里添加测试逻辑
---
+++ cases_date/测试的项目/首页/登陆/test_正确_登陆.py
@@ -0,0 +1,4 @@
+# 测试用例数据
+test_data = {'打开_url_登录': ['https://adminplus.iviewui.com/', 'nan', 'nan'], '输入_用户名': ['By.XPATH', '//*[@id="app"]/div/div[2]/div[2]/form/div[1]/div/div/div/input', 'admin'], '输入_密码': ['By.XPATH', '//*[@id="app"]/div/div[2]/div[2]/form/div[2]/div/div/div/input', 'admin'], '点击_登录按钮': ['By.XPATH', '//*[@id="app"]/div/div[2]/div[2]/form/div[4]/button', 'nan'], '断言': ['By.XPATH', '//*[@id="app"]/div/div[2]/div[1]/div/div[2]/div[1]/span/span[2]', 'Aresn'], '关闭浏览器': ['nan', 'nan', 'nan']}
+
+# 后续可以在这里添加测试逻辑
